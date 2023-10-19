e9e83715dc67d110bb83f273a4c18951861e1762
PaleLuna <robertmkrtyan334@gmail.com>
2023-10-19 20:58:31 +0300

    Реализация ComponentHolder

---
+++ Star-Wings/Assets/_Scripts/Architecture/DataHolders/Interfaces/IComponentHolder.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.Events;
+
+public interface IComponentHolder<T> where T : IGameComponent
+{
+    UnityEvent OnComponentAdded { get; }
+    
+    TP Register<TP>(TP newComponent) where TP : T;
+    void Unregister<TP>(TP component) where TP : T;
+    
+    TP Get<TP>() where TP : T;
+}
