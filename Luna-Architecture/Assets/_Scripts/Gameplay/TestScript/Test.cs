2bc7f72615d99d502d8b69b2a7dd2a0c52725dc9
PaleLuna <palelunadeveloper@gmail.com>
2023-11-09 16:32:53 +0300

    Мелкие правки кода

--- Luna-Architecture/Assets/_Scripts/Gameplay/TestScript/Test.cs
+++ Luna-Architecture/Assets/_Scripts/Gameplay/TestScript/Test.cs
@@ -1,17 +1,16 @@
 using UnityEngine;
 
-public class Test : MonoBehaviour, IUpdatable
+public class Test : MonoBehaviour, IUpdatable, IStartable
 {
-    // public void OnStart() =>
-    //     ServiceLocator.Instance.Get<GameController>()?
-    //         .updatablesHolder.Registration(this);
+    public void OnStart() =>
+        ServiceLocator.Instance.Get<GameController>()?
+            .updatablesHolder.Registration(this);
 
     public void EveryFrameRun() => 
         Debug.Log($"Run {this}");
 
-    private void OnDestroy() => 
-        ServiceLocator.Instance?.Get<GameController>()?
-            .updatablesHolder.UnRegistration(this);
-
-    
+    private void OnDestroy() =>
+        ServiceLocator.Instance
+            .Get<GameController>()
+                .updatablesHolder?.UnRegistration(this);
 }
