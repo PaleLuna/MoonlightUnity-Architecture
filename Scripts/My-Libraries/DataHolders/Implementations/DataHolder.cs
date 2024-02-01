6620417464f92ddd130d9c2eddbd8e9570a3dec3
PaleLuna <47419205+PaleLuna@users.noreply.github.com>
2024-02-01 19:05:19 +0300

    Добавил новые методы, провёл рефакторинг
    Добавил метод фильтрации + индексацию

--- Scripts/My-Libraries/DataHolders/Implementations/DataHolder.cs
+++ Scripts/My-Libraries/DataHolders/Implementations/DataHolder.cs
@@ -13,6 +13,7 @@ namespace PaleLuna.DataHolder
  */
     public class DataHolder<T> : IDataHolder<T>
     {
+        #region Properties
         /** @brief Емкость по умолчанию для списка элементов. */
         private const int DEFAULT_CAPACITY = 10;
 
@@ -27,7 +28,9 @@ namespace PaleLuna.DataHolder
 
         /** @brief Количество элементов в коллекции. */
         public int Count => _itemsList.Count;
+        #endregion
 
+        #region Constructors
         /**
         * @brief Конструктор класса.
         *
@@ -51,6 +54,7 @@ namespace PaleLuna.DataHolder
         {
             ReplaceList(list);
         }
+        #endregion
 
         #region Registration
 
@@ -156,44 +160,14 @@ namespace PaleLuna.DataHolder
             _itemsList.Remove(item);
             return item;
         }
-        #endregion
-
-        /**
-     * @brief Получение элемента по индексу.
-     *
-     * @param index Индекс элемента в коллекции.
-     * @return Элемент по указанному индексу.
-     *
-     * Пример использования:
-     * @code
-     * DataHolder<string> dataHolder = new DataHolder<string>();
-     * string item = dataHolder.At(2);
-     * @endcode
-     */
-        public T At(int index)
+        public TP Unregistration<TP>(int index)
+            where TP : T
         {
-            T res = default;
-
-            if (index >= 0 && index < _itemsList.Count)
-                res = _itemsList[index];
-
-            return res;
+            TP item = (TP)_itemsList[index];
+            _itemsList.RemoveAt(index);
+            return item;
         }
-
-        public void Clear() => _itemsList.Clear();
-
-        public void ForEach(Action<T> action) => _itemsList.ForEach(action);
-
-        /**
-     * @brief Удаление всех нулевых элементов из коллекции.
-     *
-     * Пример использования:
-     * @code
-     * DataHolder<object> dataHolder = new DataHolder<object>();
-     * dataHolder.RemoveAllNulls();
-     * @endcode
-     */
-        public void RemoveAllNulls() => _itemsList.RemoveAll(item => item == null);
+        #endregion
 
         #region MergeListMethods
 
@@ -245,13 +219,90 @@ namespace PaleLuna.DataHolder
             _itemsList = _itemsList.Union(other).ToList();
         }
 
+        #endregion
+
+        #region Access Methods
+        /**
+     * @brief Получение элемента по индексу.
+     *
+     * @param index Индекс элемента в коллекции.
+     * @return Элемент по указанному индексу.
+     *
+     * Пример использования:
+     * @code
+     * DataHolder<string> dataHolder = new DataHolder<string>();
+     * string item = dataHolder.At(2);
+     * @endcode
+     */
+        public T At(int index)
+        {
+            T res = default;
+
+            if (index >= 0 && index < _itemsList.Count)
+                res = _itemsList[index];
+
+            if(res == null)
+                Unregistration<T>(index);
+
+            return res;
+        }
+
+        #region Indexing
+        public T this[int index]
+        {
+            get => _itemsList[index];
+            set => _itemsList[index] = value;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Methods of interaction
         public T[] ToArray()
         {
             return _itemsList.ToArray();
         }
 
+        public void ForEach(Action<T> action) => _itemsList.ForEach(action);
+
+        public IDataHolder<T> Filter(Func<T, bool> predicate)
+        {
+            DataHolder<T> temp = new DataHolder<T>(this.Count);
+
+            _itemsList.ForEach(item =>
+            {
+                if (predicate(item))
+                    temp.Registration(item);
+            });
+
+            return temp;
+        }
+
+        /**
+* @brief Удаление всех нулевых элементов из коллекции.
+*
+* Пример использования:
+* @code
+* DataHolder<object> dataHolder = new DataHolder<object>();
+* dataHolder.RemoveAllNulls();
+* @endcode
+*/
+        public void RemoveAllNulls() => _itemsList.RemoveAll(item => item == null);
+
+        public void Clear() => _itemsList.Clear();
         #endregion
 
         ~DataHolder() => _itemsList.Clear();
+        public override string ToString()
+        {
+            string res = "{ ";
+
+            _itemsList.ForEach(item => res += $"{item.ToString()} ");
+
+            res += "}";
+
+            return res;
+        }
     }
 }
--- Scripts/My-Libraries/DataHolders/Interfaces/IDataHolder.cs
+++ Scripts/My-Libraries/DataHolders/Interfaces/IDataHolder.cs
@@ -4,86 +4,95 @@ using UnityEngine.Events;
 
 namespace PaleLuna.DataHolder
 {
- /**
- * @brief Интерфейс для хранения и управления коллекцией элементов.
- *
- * Этот интерфейс предоставляет методы для регистрации, удаления и обработки элементов в коллекции.
- * @tparam T Тип элемента, хранящегося в коллекции.
- */
- public interface IDataHolder<T>
- {
-  /** @brief Событие, вызываемое при добавлении нового элемента. */
-  UnityEvent<T> OnItemAdded { get; }
-
-  /** @brief Количество элементов в коллекции. */
-  int Count { get; }
-
-  /**
-    * @brief Регистрация элемента в коллекции с указанным порядком.
+    /**
+    * @brief Интерфейс для хранения и управления коллекцией элементов.
     *
-    * @tparam TP Тип элемента для регистрации.
-    * @param item Элемент для регистрации.
-    * @param order Порядок, в котором элемент будет добавлен в коллекцию.
-    * @return Зарегистрированный элемент.
+    * Этот интерфейс предоставляет методы для регистрации, удаления и обработки элементов в коллекции.
+    * @tparam T Тип элемента, хранящегося в коллекции.
     */
-  TP Registration<TP>(TP item, int order) where TP : T;
+    public interface IDataHolder<T>
+    {
+        /** @brief Событие, вызываемое при добавлении нового элемента. */
+        UnityEvent<T> OnItemAdded { get; }
 
-  /**
-     * @brief Регистрация списка элементов в коллекции.
-     *
-     * @param items Список элементов для регистрации.
-     * @param registrationType Тип регистрации (добавление в конец, в начало, замена, объединение в конец).
-     */
-  public void Registration(List<T> items, ListRegistrationType registrationType = ListRegistrationType.Replace);
+        /** @brief Количество элементов в коллекции. */
+        int Count { get; }
 
-  /**
-     * @brief Регистрация элемента в коллекции.
-     *
-     * @tparam TP Тип элемента для регистрации.
-     * @param item Элемент для регистрации.
-     * @return Зарегистрированный элемент.
-     */
-  TP Registration<TP>(TP item) where TP : T;
+        /**
+          * @brief Регистрация элемента в коллекции с указанным порядком.
+          *
+          * @tparam TP Тип элемента для регистрации.
+          * @param item Элемент для регистрации.
+          * @param order Порядок, в котором элемент будет добавлен в коллекцию.
+          * @return Зарегистрированный элемент.
+          */
+        TP Registration<TP>(TP item, int order)
+            where TP : T;
 
-  /**
-   * @brief Отмена регистрации элемента в коллекции.
-   *
-   * @tparam TP Тип элемента для отмены регистрации.
-   * @param item Элемент для отмены регистрации.
-   * @return Отмененный элемент.
-   */
-  TP Unregistration<TP>(TP item) where TP : T;
+        /**
+           * @brief Регистрация списка элементов в коллекции.
+           *
+           * @param items Список элементов для регистрации.
+           * @param registrationType Тип регистрации (добавление в конец, в начало, замена, объединение в конец).
+           */
+        public void Registration(
+            List<T> items,
+            ListRegistrationType registrationType = ListRegistrationType.Replace
+        );
 
-  /**
-     * @brief Получение элемента по индексу.
-     *
-     * @param index Индекс элемента в коллекции.
-     * @return Элемент по указанному индексу.
-     */
-  T At(int index);
+        /**
+           * @brief Регистрация элемента в коллекции.
+           *
+           * @tparam TP Тип элемента для регистрации.
+           * @param item Элемент для регистрации.
+           * @return Зарегистрированный элемент.
+           */
+        TP Registration<TP>(TP item)
+            where TP : T;
 
-  /** @brief Очистка коллекции. */
-  public void Clear();
+        /**
+         * @brief Отмена регистрации элемента в коллекции.
+         *
+         * @tparam TP Тип элемента для отмены регистрации.
+         * @param item Элемент для отмены регистрации.
+         * @return Отмененный элемент.
+         */
+        TP Unregistration<TP>(TP item)
+            where TP : T;
+        TP Unregistration<TP>(int index) where TP : T;
 
-  /**
-    * @brief Применение действия ко всем элементам коллекции.
-    *
-    * @param action Действие, применяемое к каждому элементу коллекции.
+        /**
+           * @brief Получение элемента по индексу.
+           *
+           * @param index Индекс элемента в коллекции.
+           * @return Элемент по указанному индексу.
+           */
+        T At(int index);
+
+        IDataHolder<T> Filter(Func<T, bool> predicate);
+
+        /** @brief Очистка коллекции. */
+        public void Clear();
+
+        /**
+          * @brief Применение действия ко всем элементам коллекции.
+          *
+          * @param action Действие, применяемое к каждому элементу коллекции.
+          */
+        void ForEach(Action<T> action);
+
+        /** @brief Преобразование коллекции в массив. */
+        T[] ToArray();
+    }
+
+    /**
+    * @brief Перечисление для типов регистрации элементов в коллекции.
     */
-  void ForEach(Action<T> action);
-
-  /** @brief Преобразование коллекции в массив. */
-  T[] ToArray();
- }
-
- /**
- * @brief Перечисление для типов регистрации элементов в коллекции.
- */
- public enum ListRegistrationType
- {
-  AddToEnd,
-  AddToStart,
-  Replace,
-  MergeToEndUnion
- }
+    public enum ListRegistrationType
+    {
+        AddToEnd,
+        AddToStart,
+        Replace,
+        MergeToEndUnion
+    }
 }
