d7de149079446986213bbe67f0282cc523c28e0c
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-10-10 14:34:05 +0300

    Update SceneLoaderService.cs
    Добавлена возможность асинхронной загрузки
    Улучшено взаимодействие с Baggage

--- Scripts/Architecture/Services/Implementations/SceneService/SceneLoaderService.cs
+++ Scripts/Architecture/Services/Implementations/SceneService/SceneLoaderService.cs
@@ -1,10 +1,25 @@
-﻿using UnityEngine.SceneManagement;
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace PaleLuna.Architecture.Services
 {
     public class SceneLoaderService : IService
     {
-        private SceneBaggage _sceneBaggage = null;
+        private SceneBaggage _nextSceneBaggage = null;
+        private SceneBaggage _currentSceneBaggage = null;
+
+        private AsyncOperation _asyncOperation;
+
+        public bool sceneIsLoad
+        {
+            get
+            {
+                if(_asyncOperation == null)
+                    return false;
+                return _asyncOperation.isDone;
+            }
+        }
         
         public void LoadScene(int sceneNum)
         {
@@ -13,18 +28,51 @@ namespace PaleLuna.Architecture.Services
 
         public void LoadScene(string sceneName)
         {
+            SwapBaggage();
             SceneManager.LoadScene(sceneName);
         }
 
-        public SceneLoaderService SetBaggage(SceneBaggage sceneBaggage)
+        public void LoadSceneAsync(int sceneNum, bool allowSceneActivation = true)
         {
-            _sceneBaggage = sceneBaggage;
+            SwapBaggage();
+            _asyncOperation = SceneManager.LoadSceneAsync(sceneNum);
+
+            _asyncOperation.allowSceneActivation = allowSceneActivation;
+        }
+        public void AllowNextScene()
+        {
+            if (_asyncOperation != null) _asyncOperation.allowSceneActivation = true;
+
+            _asyncOperation = null;
+        }
+
+        public SceneLoaderService SetNextBaggage(SceneBaggage sceneBaggage)
+        {
+            _nextSceneBaggage = sceneBaggage;
             return this;
         }
 
-        public SceneBaggage GetBaggage()
+        public SceneBaggage GetNextBaggage()
         {
-            return _sceneBaggage;
+            if (_nextSceneBaggage == null)
+                _nextSceneBaggage = new();
+
+            return _nextSceneBaggage;
         }
+
+        public SceneBaggage GetCurrentBaggage()
+        {
+            if (_currentSceneBaggage == null)
+                throw new NullReferenceException("current scene baggage is null");
+
+            return _currentSceneBaggage;
+        }
+
+        private void SwapBaggage()
+        {
+            _currentSceneBaggage = _nextSceneBaggage;
+            _nextSceneBaggage = null;
+        }
+
     }
 }
