d4defd8d0136c19ff0f10c106910b5ff509c3bfc
TwillightMoon <47419205+TwillightMoon@users.noreply.github.com>
2024-10-11 14:42:39 +0300

    Удаление комментариев

--- Scripts/Architecture/Game/GameLoops.cs
+++ Scripts/Architecture/Game/GameLoops.cs
@@ -8,44 +8,16 @@ using UnityEngine;
 
 namespace PaleLuna.Architecture.Loops
 {
-    /**
-     * @brief Класс GameController управляет различными аспектами игры.
-     * 
-     * GameController является главным контроллером игры, ответственным за управление обновлением, паузой и состояниями игры.
-     * Реализует интерфейсы IService и IStartable, что позволяет его использование в качестве сервиса и компонента, который может быть инициализирован при старте.
-     */
     public class GameLoops : MonoBehaviour, IService, IStartable
     {
-        /**
-        * @brief Флаг, указывающий, был ли GameController успешно инициализирован.
-        */
         private bool _isStarted;
 
-        /**
-        * @brief Хранилище компонентов, поддерживающих паузу.
-        */
         public DataHolder<IPausable> pausablesHolder { get; private set; }
-
-        /**
-        * @brief Объект для хранения и управления обновляемыми компонентами.
-        */
         public UpdatablesHolder updatablesHolder { get; private set; }
-
-        /**
-         * @brief Хранилище состояний игры.
-         */
         public StateHolder<GameStateBase> stateHolder { get; private set; }
 
-        /**
-         * @brief Получает значение, указывающее, был ли GameController успешно инициализирован.
-         */
         public bool IsStarted => _isStarted;
 
-        /**
-         * @brief Метод, вызываемый при старте GameController.
-         * 
-         * Метод OnStart инициализирует GameController, создавая необходимые хранилища и объекты для управления состоянием игры.
-         */
         public void OnStart()
         {
             if (_isStarted) return;
@@ -80,36 +52,24 @@ namespace PaleLuna.Architecture.Loops
 
         #region MonoEvents
 
-        /**
-         * @brief Метод, вызываемый в каждом кадре для обновления обновляемых компонентов.
-         */
         private void Update()
         {
             updatablesHolder.everyFrameUpdatablesHolder
                 .ForEach(updatable => updatable.EveryFrameRun());
         }
 
-        /**
-         * @brief Метод, вызываемый в каждом FixedUpdate для обновления компонентов, требующих фиксированную частоту обновления.
-         */
         private void FixedUpdate()
         {
             updatablesHolder.fixedUpdatablesHolder
                 .ForEach(updatable => updatable.FixedFrameRun());
         }
 
-        /**
-         * @brief Метод, вызываемый в каждом LateUpdate для обновления компонентов, требующих обновление после Update.
-         */
         private void LateUpdate()
         {
             updatablesHolder.lateUpdatablesHolder
                 .ForEach(updatable => updatable.LateUpdateRun());
         }
 
-        /**
-         * @brief Метод, вызываемый в каждом такте игрового времени для обновления компонентов с использованием метода EveryTickRun.
-         */
         private void Tick()
         {
             updatablesHolder.tickUpdatableHolder
